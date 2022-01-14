e60f71458ce18a70d0b3cc9f817ed2c38baf408d
Gabriel Tuzzo <76889705+GabrielTuzzo@users.noreply.github.com>
2022-01-14 17:53:22 +0000

    Initial commit

--- responsive-dashboard-with -darkmode-for-different-displays/index.js
+++ responsive-dashboard-with -darkmode-for-different-displays/index.js
@@ -1,8 +1,8 @@
 const themeButton = document.querySelector("#theme-selector")
 
 themeButton.addEventListener('click', function () {
-    document.body.classList.toggle('dark-theme') //add and remove the class name, like "document.body.classList.remove('dark-theme-variables')
-    
+    document.body.classList.toggle('dark-theme') 
+    //add and remove the class name    
     themeButton.querySelector('.fa-moon').classList.toggle('selected')
     themeButton.querySelector('.fa-sun').classList.toggle('selected')
 }) 
